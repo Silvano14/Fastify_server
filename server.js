067d2e06108fbd4e2495a339c234c892f47036a3
Silvano <silvano.norberti@btinkeeng.com>
2023-01-12 16:17:24 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true });
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+const fastify = Fastify()
+await fastify.register(cors, {
+  // put your options here
+})
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
