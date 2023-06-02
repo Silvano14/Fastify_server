8cdcaa5e3e76da6105a33bdbb6a9a2eb4c70a1b4
Silvano <s.norberti@dstech.it>
2023-06-02 16:38:17 +0200

    Removed accept by header in the fetch

--- fetch.js
+++ fetch.js
@@ -1,7 +1,6 @@
 fetch("http://localhost:3000/login", {
   method: "POST",
   headers: {
-    Accept: "application/json",
     "Content-Type": "application/json",
   },
   body: JSON.stringify({ username: "High ev merda", password: "provolone" }),
